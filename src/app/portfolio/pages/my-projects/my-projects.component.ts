e289514f7e51e0b5af8b4247d2cdf5b83a15e0a5
JuanTasayco <juantasayco266@gmail.com>
2023-06-28 20:21:37 -0500

    beforeNgPrime2

--- src/app/portfolio/pages/my-projects/my-projects.component.ts
+++ src/app/portfolio/pages/my-projects/my-projects.component.ts
@@ -15,7 +15,7 @@ export class MyProjectsComponent implements OnInit {
         'Aplicación responsive que consume la API de Spotify para mostrar los artistas top y escuchar algunas canciones. RTambién utiliza un pequeño back en node js para actualizar el token de spofity automaticamente(ya que tiene caducidad de una hora)',
       imagen: 'hola',
       tecnologias: 'Angular, Nest Js, SASS',
-      repositorio: 'link:httpsdasdsagithub.com',
+      repositorio: 'https://github.com/JuanTasayco/FM4-SpotyAPIOnlyFrontend',
       link: 'www.paginaSpotify.com',
     },
     {
@@ -23,30 +23,43 @@ export class MyProjectsComponent implements OnInit {
       description:
         'Aplicación responsive que consume la API de Mapbox para calcular distancias entre dos puntos ( generalmente tu ubicación con el lugar donde irás), encontrar lugares específicos, etc',
       imagen: 'hola',
+      repositorio: 'https://github.com/JuanTasayco/PF4-MapsAngular',
+      link: 'www.paginaSpotify.com',
     },
     {
       title: 'Lpm App V1',
       description:
         'Aplicación responsive con data propia que permite visualizar información de una guía de contabilidad para LPM, cuenta con características muy simples y fáciles de usar para el usuario, esta es la primera versión (solo front).',
       imagen: 'hola',
+      repositorio:
+        'https://github.com/JuanTasayco/06-WebConta-Angular/tree/main/src',
+      link: 'www.paginaSpotify.com',
     },
     {
       title: 'WebEntertaiment',
       description:
         'Aplicación responsive full stack de películas, permite obtener un catálogo de películas, añadir tus películas a favoritas y explorar más opciones.',
       imagen: 'hola',
+      repositorio:
+        'https://github.com/JuanTasayco/PF3-WebAppEntertainment , https://github.com/JuanTasayco/BF3-WebAppEntertainment ',
+      link: 'www.paginaSpotify.com',
     },
     {
       title: 'Lpm App V2',
       description:
         'Aplicación responsive full stack, esta es la segunda versión de la app de LPM, añade un panel de administradores al cual solo se accederá mediante la autenticación y autorización debida, puede modificar, agregar y eliminar guías, también puede eliminar,editar y eliminar usuarios, todo esto mencionado como caracteristica adicional a la visualización que solo tenía la versión 1.',
       imagen: 'hola',
+      repositorio:
+        'https://github.com/JuanTasayco/F1-LpmGuideFrontend , https://github.com/JuanTasayco/F1-LpmGuideBackend ',
+      link: 'www.paginaSpotify.com',
     },
     {
       title: 'FilterMaps',
       description:
         'Aplicación responsive que permite encontrar un país o un serie de países en base a filtros ( por continente, por nombre, etc), también utilizo Mapbox para mostrar la ubicación exacta del país en el mapa',
       imagen: 'hola',
+      repositorio: 'https://github.com/JuanTasayco/PF5-CountryAPI',
+      link: 'www.paginaSpotify.com',
     },
   ];
   ngOnInit(): void {}
