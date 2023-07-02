0fe27ba3f1275ea82979d65e4f31424c1f968793
JuanTasayco <juantasayco266@gmail.com>
2023-07-02 17:46:34 -0500

    updateSectionsProject

--- src/app/portfolio/components/project-description/project-description.component.ts
+++ src/app/portfolio/components/project-description/project-description.component.ts
@@ -18,52 +18,20 @@ export interface ProjectDescriptions {
 export class ProjectDescriptionComponent implements OnInit {
   constructor() {}
   @Input('contenido') contentProjects: ProjectDescriptions[] = [];
-  arreglo : any [] = [
-    {
-        itemImageSrc: 'https://primefaces.org/cdn/primeng/images/galleria/galleria1.jpg',
-        thumbnailImageSrc: 'https://primefaces.org/cdn/primeng/images/galleria/galleria1s.jpg',
-        alt: 'Description for Image 1',
-        title: 'Title 1'
-    },
-    {
-        itemImageSrc: 'https://primefaces.org/cdn/primeng/images/galleria/galleria2.jpg',
-        thumbnailImageSrc: 'https://primefaces.org/cdn/primeng/images/galleria/galleria2s.jpg',
-        alt: 'Description for Image 2',
-        title: 'Title 2'
-    },
-    {
-        itemImageSrc: 'https://primefaces.org/cdn/primeng/images/galleria/galleria3.jpg',
-        thumbnailImageSrc: 'https://primefaces.org/cdn/primeng/images/galleria/galleria3s.jpg',
-        alt: 'Description for Image 3',
-        title: 'Title 3'
-    },
-    {
-        itemImageSrc: 'https://primefaces.org/cdn/primeng/images/galleria/galleria4.jpg',
-        thumbnailImageSrc: 'https://primefaces.org/cdn/primeng/images/galleria/galleria4s.jpg',
-        alt: 'Description for Image 4',
-        title: 'Title 4'
-    },
-    {
-        itemImageSrc: 'https://primefaces.org/cdn/primeng/images/galleria/galleria5.jpg',
-        thumbnailImageSrc: 'https://primefaces.org/cdn/primeng/images/galleria/galleria5s.jpg',
-        alt: 'Description for Image 5',
-        title: 'Title 5'
-    },]
-
 
   responsiveOptions: any[] = [
     {
-        breakpoint: '1024px',
-        numVisible: 5
+      breakpoint: '1024px',
+      numVisible: 5,
     },
     {
-        breakpoint: '768px',
-        numVisible: 3
+      breakpoint: '768px',
+      numVisible: 3,
     },
     {
-        breakpoint: '560px',
-        numVisible: 1
-    }
-];
+      breakpoint: '560px',
+      numVisible: 1,
+    },
+  ];
   ngOnInit(): void {}
 }
--- src/app/portfolio/pages/my-projects/my-projects.component.ts
+++ src/app/portfolio/pages/my-projects/my-projects.component.ts
@@ -10,6 +10,82 @@ export class MyProjectsComponent implements OnInit {
   constructor() {}
   public infoProject: ProjectDescriptions[] = [
     {
+      title: 'Lpm App V1',
+      description:
+        'Aplicación responsive con data propia que permite visualizar información de una guía de contabilidad para LPM, cuenta con características muy simples y fáciles de usar para el usuario, esta es la primera versión (solo front).',
+      imagenes: [
+        {
+          itemImageSrc: 'assets/imagenes/lpmOne1.png',
+          alt: 'lpmv1Img1',
+        },
+        {
+          itemImageSrc: 'assets/imagenes/lpmOne2.png',
+          alt: 'lpmv1Img2',
+        },
+        {
+          itemImageSrc: 'assets/imagenes/lpmOne3.png',
+          alt: 'lpmv1Img3',
+        },
+        {
+          itemImageSrc: 'assets/imagenes/lpmOne4.png',
+          alt: 'lpmv1Img4',
+        },
+      ],
+      repositorio:
+        'https://github.com/JuanTasayco/06-WebConta-Angular/tree/main/src',
+      link: 'www.paginaSpotify.com',
+    },
+    {
+      title: 'Lpm App V2',
+      description:
+        'Aplicación responsive full stack, esta es la segunda versión de la app de LPM, añade un panel de administradores al cual solo se accederá mediante la autenticación y autorización debida, puede modificar, agregar y eliminar guías, también puede eliminar,editar y eliminar usuarios, todo esto mencionado como caracteristica adicional a la visualización que solo tenía la versión 1.',
+      imagenes: [
+        {
+          itemImageSrc: 'assets/imagenes/lpmTwo2.png',
+          alt: 'lpmv1Img2',
+        },
+        {
+          itemImageSrc: 'assets/imagenes/lpmTwo3.png',
+          alt: 'lpmv1Img3',
+        },
+        {
+          itemImageSrc: 'assets/imagenes/lpmTwo4.png',
+          alt: 'lpmv1Img4',
+        },
+        {
+          itemImageSrc: 'assets/imagenes/lpmTwo5.png',
+          alt: 'lpmv1Img4',
+        },
+        {
+          itemImageSrc: 'assets/imagenes/lpmTwo6.png',
+          alt: 'lpmv1Img1',
+        },
+        {
+          itemImageSrc: 'assets/imagenes/lpmTwo7.png',
+          alt: 'lpmv1Img2',
+        },
+        {
+          itemImageSrc: 'assets/imagenes/lpmTwo8.png',
+          alt: 'lpmv1Img3',
+        },
+        {
+          itemImageSrc: 'assets/imagenes/lpmTwo9.png',
+          alt: 'lpmv1Img4',
+        },
+        {
+          itemImageSrc: 'assets/imagenes/lpmTwo10.png',
+          alt: 'lpmv1Img3',
+        },
+        {
+          itemImageSrc: 'assets/imagenes/lpmTwo1.png',
+          alt: 'lpmv1Img4',
+        },
+      ],
+      repositorio:
+        'https://github.com/JuanTasayco/F1-LpmGuideFrontend , https://github.com/JuanTasayco/F1-LpmGuideBackend ',
+      link: 'www.paginaSpotify.com',
+    },
+    {
       title: 'Spotify API',
       description:
         'Aplicación responsive que consume la API de Spotify para mostrar los artistas top y escuchar algunas canciones. RTambién utiliza un pequeño back en node js para actualizar el token de spofity automaticamente(ya que tiene caducidad de una hora)',
@@ -60,50 +136,38 @@ export class MyProjectsComponent implements OnInit {
       repositorio: 'https://github.com/JuanTasayco/PF4-MapsAngular',
       link: 'www.paginaSpotify.com',
     },
-    {
-      title: 'Lpm App V1',
-      description:
-        'Aplicación responsive con data propia que permite visualizar información de una guía de contabilidad para LPM, cuenta con características muy simples y fáciles de usar para el usuario, esta es la primera versión (solo front).',
-      imagenes: [
-        {
-          itemImageSrc: 'assets/imagenes/lpmOne1.png',
-          alt: 'lpmv1Img1',
-        },
-        {
-          itemImageSrc: 'assets/imagenes/lpmOne2.png',
-          alt: 'lpmv1Img2',
-        },
-        {
-          itemImageSrc: 'assets/imagenes/lpmOne3.png',
-          alt: 'lpmv1Img3',
-        },
-        {
-          itemImageSrc: 'assets/imagenes/lpmOne4.png',
-          alt: 'lpmv1Img4',
-        },
-      ],
-      repositorio:
-        'https://github.com/JuanTasayco/06-WebConta-Angular/tree/main/src',
-      link: 'www.paginaSpotify.com',
-    },
+
     {
       title: 'WebEntertaiment',
       description:
         'Aplicación responsive full stack de películas, permite obtener un catálogo de películas, añadir tus películas a favoritas y explorar más opciones.',
-      imagenes: [],
+      imagenes: [
+        {
+          itemImageSrc: 'assets/imagenes/lpmTwo2.png',
+          alt: 'lpmv1Img2',
+        },
+        {
+          itemImageSrc: 'assets/imagenes/lpmTwo3.png',
+          alt: 'lpmv1Img3',
+        },
+        {
+          itemImageSrc: 'assets/imagenes/lpmTwo4.png',
+          alt: 'lpmv1Img4',
+        },
+        {
+          itemImageSrc: 'assets/imagenes/lpmTwo5.png',
+          alt: 'lpmv1Img4',
+        },
+        {
+          itemImageSrc: 'assets/imagenes/lpmTwo6.png',
+          alt: 'lpmv1Img1',
+        },
+      ],
       repositorio:
         'https://github.com/JuanTasayco/PF3-WebAppEntertainment , https://github.com/JuanTasayco/BF3-WebAppEntertainment ',
       link: 'www.paginaSpotify.com',
     },
-    {
-      title: 'Lpm App V2',
-      description:
-        'Aplicación responsive full stack, esta es la segunda versión de la app de LPM, añade un panel de administradores al cual solo se accederá mediante la autenticación y autorización debida, puede modificar, agregar y eliminar guías, también puede eliminar,editar y eliminar usuarios, todo esto mencionado como caracteristica adicional a la visualización que solo tenía la versión 1.',
-      imagenes: [],
-      repositorio:
-        'https://github.com/JuanTasayco/F1-LpmGuideFrontend , https://github.com/JuanTasayco/F1-LpmGuideBackend ',
-      link: 'www.paginaSpotify.com',
-    },
+
     {
       title: 'FilterMaps',
       description:
