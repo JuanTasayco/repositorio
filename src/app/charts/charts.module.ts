7a74c8544a7db0368db373299625f703d9cacdf8
JuanTasayco <juantasayco266@gmail.com>
2023-07-13 13:34:17 -0500

    addNgxChartInstallation

--- src/app/charts/charts.module.ts
+++ src/app/charts/charts.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PieChartComponent } from './pie-chart/pie-chart.component';
 import { NgChartsConfiguration, NgChartsModule } from 'ng2-charts';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { HorizontalChartComponent } from './horizontal-chart/horizontal-chart.component';
 
 @NgModule({
-  declarations: [PieChartComponent],
-  imports: [CommonModule, NgChartsModule],
-  exports: [PieChartComponent, NgChartsModule],
-  providers: [
-
-  ],
+  declarations: [PieChartComponent, HorizontalChartComponent],
+  imports: [CommonModule, NgxChartsModule],
+  exports: [PieChartComponent],
 })
 export class ChartsModule {}
---
+++ src/app/charts/horizontal-chart/horizontal-chart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-horizontal-chart',
+  templateUrl: './horizontal-chart.component.html',
+  styleUrls: ['./horizontal-chart.component.scss']
+})
+export class HorizontalChartComponent {
+
+}
