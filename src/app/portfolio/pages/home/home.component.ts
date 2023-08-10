3d500c99f273a7a2c71a80f791282619a236b0d7
Juan Tasayco <juantasayco266@gmail.com>
2023-08-10 18:17:25 -0500

    firstDates

--- src/app/portfolio/pages/home/home.component.ts
+++ src/app/portfolio/pages/home/home.component.ts
@@ -18,17 +18,12 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit, AfterViewInit {
-  @ViewChildren('letterPresent') letterPresent!: QueryList<ElementRef>;
-  @ViewChildren('letterName') letterName!: QueryList<ElementRef>;
-  @ViewChild('parrafoTitle') parrafoTitle!: ElementRef<HTMLElement>;
-  @ViewChild('homeContainer') homeContainer!: ElementRef<HTMLElement>;
   lenis: any;
   ngOnInit(): void {
     /* declaracion lenin  */
     this.lenis = new Lenis({
-      lerp: 2,
       smoothWheel: true,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      duration: 0,
     });
 
     const raf = (time: any) => {
@@ -44,80 +39,5 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
   constructor() {}
 
-  ngAfterViewInit(): void {
-    /* INICIO ANIMACIONES */
-    const nav = document.querySelector('#nav');
-    gsap.from(nav, {
-      yPercent: -100,
-      opacity: 0,
-    });
-
-    let tl = gsap.timeline({ duration: 1 });
-    tl.timeScale(8);
-    this.letterPresent.forEach((element, index) => {
-      tl.from(element.nativeElement, {
-        duration: 1,
-        opacity: 1,
-        y: '100%',
-        ease: 'power4.out',
-      });
-    });
-
-    let tl2 = gsap.timeline({});
-    this.letterName.forEach((element, index) => {
-      tl2.from(element.nativeElement, {
-        opacity: 0,
-        yPercent: 100,
-      });
-    });
-
-    tl2.fromTo(
-      this.parrafoTitle.nativeElement,
-      {
-        opacity: 0,
-        y: '-100%',
-      },
-      {
-        opacity: 1,
-        y: '0%',
-        duration: 1,
-        ease: 'power1.out',
-      }
-    );
-
-    /* FIN ANIMACIONES INICIO */
-    const continueArrow = document.querySelector('#continueArrow');
-    const timeline3 = gsap.timeline({
-      scrollTrigger: {
-        trigger: this.homeContainer.nativeElement,
-        markers: true,
-        start: 'top top',
-        end: 'center',
-        scrub: true,
-        toggleActions: 'play none none none',
-      },
-    });
-
-    timeline3.to(continueArrow, {
-      y: '-100%',
-    });
-
-    this.letterPresent.forEach((element) => {
-      timeline3.to(element.nativeElement, {
-        opacity: 0,
-        ease: 'power3.out',
-        y: '-105%',
-        duration: 1,
-      });
-    });
-
-    this.letterName.forEach((element) => {
-      timeline3.to(element.nativeElement, {
-        opacity: 0,
-        ease: 'power3.out',
-        y: '-105%',
-        duration: 1,
-      });
-    });
-  }
+  ngAfterViewInit(): void {}
 }
