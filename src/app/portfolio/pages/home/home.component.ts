dd6dfe0c0f68991f64b7bb1b3d2759740f4f91fc
Juan Tasayco <juantasayco266@gmail.com>
2023-08-11 17:26:03 -0500

    avance2

--- src/app/portfolio/pages/home/home.component.ts
+++ src/app/portfolio/pages/home/home.component.ts
@@ -2,13 +2,12 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
-  HostListener,
   OnInit,
   QueryList,
   ViewChild,
   ViewChildren,
 } from '@angular/core';
-import { Scroll } from '@angular/router';
+
 import Lenis from '@studio-freight/lenis';
 
 import { gsap } from 'gsap';
@@ -18,6 +17,9 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit, AfterViewInit {
+  @ViewChild('coverTitle') coverTitle!: ElementRef<HTMLElement>;
+  @ViewChild('containerGrid') containerGrid!: ElementRef<HTMLElement>;
+  @ViewChildren('figure') figuresIcon!: QueryList<ElementRef>;
   lenis: any;
   ngOnInit(): void {
     /* declaracion lenin  */
@@ -25,19 +27,52 @@ export class HomeComponent implements OnInit, AfterViewInit {
       smoothWheel: true,
       duration: 0,
     });
-
     const raf = (time: any) => {
       this.lenis.raf(time);
       requestAnimationFrame(raf);
     };
     requestAnimationFrame(raf);
-
-    /* final de declaracion */
     /* declaracion de gsap (es necesario que esté después de lenin) */
     gsap.registerPlugin(ScrollTrigger);
   }
 
-  constructor() {}
+  ngAfterViewInit(): void {
+    /* animacion pin titleText */
 
-  ngAfterViewInit(): void {}
+    gsap.to(this.coverTitle.nativeElement, {
+      scrollTrigger: {
+        trigger: this.containerGrid.nativeElement,
+        pin: this.coverTitle.nativeElement,
+        end: '70%',
+        pinSpacing: false,
+        scrub: true,
+      },
+    });
+
+    /* fin animacion pinTitleText */
+
+    this.figuresIcon.forEach((icon) => {
+      gsap.from(icon.nativeElement, {
+        scale: 0.3,
+      });
+    });
+
+    const tlFigures = gsap.timeline({
+      scrollTrigger: {
+        trigger: this.containerGrid.nativeElement,
+        scrub: true,
+        markers: true,
+        start: 'top top',
+        end: '80% center',
+      },
+    });
+
+    this.figuresIcon.forEach((icon) => {
+      tlFigures.to(icon.nativeElement, {
+        scale: 0.2,
+      });
+    });
+  }
+
+  constructor() {}
 }
